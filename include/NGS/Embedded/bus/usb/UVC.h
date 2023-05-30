8c87e6e08307f72fd59c0fc99c9fbec58864cff2
situNagisa <1300296933@qq.com>
2023-05-30 16:43:01 +0800

    uvc

---
+++ include/NGS/Embedded/bus/usb/UVC.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "NGS/NGS_Defined.h"
+#include "NGS/NGS_STL.h"
+#include "NGS/NGS_Bit.h"
+#include "NGS/Embedded/bus/Bus.h"
+
+NGS_BEGIN
+
+class UVC : public Bus {
+public:
+
+public:
+
+private:
+
+private:
+
+};
+
+NGS_END
