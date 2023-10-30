a1db7c19b82e270cd0c4b8e549d9d9ea004a5298
situNagisa <1300296933@qq.com>
2023-10-30 22:04:37 +0800

    claer

--- test/test.cpp
+++
@@ -1,6 +0,0 @@
-#include "NGS/base/base.h"
-
-int main(){
-	NGS_LOGL(debug, "hello cmake");
-	return 0;
-}
--- unit_test/main.cpp
+++
@@ -1,7 +0,0 @@
-﻿#include "defined.h"
-
-int main(int argc, char** argv)
-{
-	testing::InitGoogleTest(&argc, argv);
-	return RUN_ALL_TESTS();
-}
