b05ac20f588c371df70d92cf8ed82ee40dd1f0ff
situNagisa <1300296933@qq.com>
2023-12-03 17:06:29 +0800

    更新 container.h

--- include/NGS/external/stl/range/transform/container.h
+++ include/NGS/external/stl/range/transform/container.h
@@ -4,11 +4,13 @@
 
 NGS_LIB_MODULE_BEGIN
 
+template<class _Type = void>
 constexpr decltype(auto) as_vector(::std::ranges::sized_range auto&& range)
 {
-	using range_type = std::vector<::std::ranges::range_value_t<decltype(range)>>;
+	using value_type = std::conditional_t<::std::is_void_v<_Type>, ::std::ranges::range_value_t<decltype(range)>, _Type>;
+	using range_type = std::vector<value_type>;
 	range_type result{ ::std::ranges::size(range) };
-	::std::ranges::transform(range, result.begin(), [](auto&& value) { return ::std::forward<decltype(value)>(value); });
+	::std::ranges::transform(NGS_PP_PERFECT_FORWARD(range), result.begin(), [](auto&& value) { return NGS_PP_PERFECT_FORWARD(value); });
 	return result;
 }
 
