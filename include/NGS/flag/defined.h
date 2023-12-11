34ec32b838661e11cdb19338eb6caab44bb23c20
situNagisa <1300296933@qq.com>
2023-12-11 09:28:11 +0800

    0.1.0
    用于仿造vulkan的思路

---
+++ include/NGS/flag/defined.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+
+#include "NGS/basic/basic.h"
+#include "NGS/mpl/mpl.h"
+#include "NGS/bit/bit.h"
+
+#define NGS_LIB_VERSION_MAJOR 0
+#define NGS_LIB_VERSION_MINOR 1
+#define NGS_LIB_VERSION_PATCH 0
+#define NGS_LIB_NAME flags
+#include "NGS/lib/lib.h"
