e743f35f87c85b0f4bf17cb72c79811f411bab86
situNagisa <1300296933@qq.com>
2024-01-01 20:50:40 +0800

    0.1.0
    提供一个可以用于非类型模板参数的binder

---
+++ include/NGS/functional/binder/core/bind.h
@@ -0,0 +1,33 @@
+﻿#pragma once
+
+#include "./binder.h"
+#include "./defined.h"
+
+NGS_LIB_MODULE_BEGIN
+
+constexpr auto bind(auto&& functor, parameter_packet::packet_like auto&& packet)
+{
+	return binder<decltype(functor), decltype(packet)>(NGS_PP_PERFECT_FORWARD(functor), NGS_PP_PERFECT_FORWARD(packet));
+}
+
+constexpr auto bind(auto&& functor, auto&&... args)
+{
+	return NGS_LIB_MODULE_NAME::bind(NGS_PP_PERFECT_FORWARD(functor), parameter_packet::make_packet(NGS_PP_PERFECT_FORWARD(args)...));
+}
+
+namespace _detail
+{
+	template<size_t _Offset, size_t... _Index>
+	constexpr decltype(auto) bind_on_sequence(::std::index_sequence<_Index...>, auto&& functor, auto&&... args)
+	{
+		return NGS_LIB_MODULE_NAME::bind(NGS_PP_PERFECT_FORWARD(functor), NGS_PP_PERFECT_FORWARD(args)..., parameter_packet::placeholders::n<_Index + _Offset + 1>...);
+	}
+}
+
+template<size_t _Sequence, size_t _Offset = 0>
+constexpr decltype(auto) bind(auto&& functor, auto&&... args)
+{
+	return _detail::bind_on_sequence<_Offset>(::std::make_index_sequence<_Sequence>(), NGS_PP_PERFECT_FORWARD(functor), NGS_PP_PERFECT_FORWARD(args)...);
+}
+
+NGS_LIB_MODULE_END
---
+++ include/NGS/functional/binder/core/replace.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+
+#include "./binder.h"
+
+NGS_LIB_MODULE_BEGIN
+
+constexpr decltype(auto) replace(is_binder auto&& binder, auto&&... args)
+{
+	return NGS_PP_PERFECT_FORWARD(binder)(NGS_PP_PERFECT_FORWARD(args)...);
+}
+
+NGS_LIB_MODULE_END
